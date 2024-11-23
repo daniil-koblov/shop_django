2aa7df5b141e07f92b298bb7f1ecd1e6d5c1d0b2
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2024-11-23 03:15:36 +0300

    Start upload to server.

--- shop_project/shop_project/settings.py
+++ shop_project/shop_project/settings.py
@@ -21,12 +21,21 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pl6w42tgjcq)y)xa%q!eckke%%bwq6w49hbm%wqy*ji5+vjft9'
+SECRET_KEY = os.getenv('SECRET_KEY')
+# 'django-insecure-pl6w42tgjcq)y)xa%q!eckke%%bwq6w49hbm%wqy*ji5+vjft9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
 
 ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'anymoreshop.pythonanywhere.com',
+]
+
+INTERNAL_IPS = [
     '127.0.0.1',
 ]
 
@@ -41,9 +50,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shopapp',
+    # 'debug_toolbar',
 ]
 
 MIDDLEWARE = [
+    # 'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -81,8 +92,15 @@ WSGI_APPLICATION = 'shop_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'anymoreshop$default',
+        'USER': 'anymoreshop',
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': 'anymoreshop.mysql.pythonanywhere-services.com',
+        'OPTIONS': {
+            'init_command': "SET NAMES 'utf8mb4';SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        },
     }
 }
 
@@ -122,6 +140,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static/'
+
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
--- shop_project/shop_project/urls.py
+++ shop_project/shop_project/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("shopapp.urls"), name="shopapp"),
+    # path('__debug__/', include("debug_toolbar.urls")),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
